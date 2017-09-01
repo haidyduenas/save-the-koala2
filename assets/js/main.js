d528d845253e345cdf5442c3c5ff193b35ef77b8
haidy <haidy>
2017-09-01 15:49:22 -0500

    Evaluado

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $("body").tooltip({ selector: '[data-toggle=tooltip]' });
+});
+$(document).on('hidden.bs.modal', function (e) {
+    var target = $(e.target);
+    target.removeData('bs.modal')
+    .find(".modal-body").html('');
+});
